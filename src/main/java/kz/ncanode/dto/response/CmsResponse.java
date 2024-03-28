6b68df11fe8d1b02a516ee97f1517115bbdd3bb7
Medeu Amangeldi <medcod49@gmail.com>
2024-03-27 21:53:46 -0500

    feat: multiple signs create

--- src/main/java/kz/ncanode/dto/response/CmsResponse.java
+++ src/main/java/kz/ncanode/dto/response/CmsResponse.java
@@ -4,6 +4,7 @@ import lombok.Data;
 import lombok.EqualsAndHashCode;
 import lombok.experimental.SuperBuilder;
 import lombok.extern.jackson.Jacksonized;
+import java.util.List;
 
 @Jacksonized
 @EqualsAndHashCode(callSuper = true)
